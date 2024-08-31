d596d6b661f09efc7c8b006f0936ec387a04f156
AIMEN BOUKHARI <129920708+aymen-000@users.noreply.github.com>
2024-08-31 22:23:49 +0200

    Update config.js

--- api/config.js
+++ api/config.js
@@ -1,3 +1,3 @@
-var passoword = "gZLs7VxmxpS34VAa"
-const url = "mongodb+srv://aymne011:gZLs7VxmxpS34VAa@booking.hmhexwv.mongodb.net/?retryWrites=true&w=majority"
+var passoword = PASSW
+const url = URL
 module.exports = url
